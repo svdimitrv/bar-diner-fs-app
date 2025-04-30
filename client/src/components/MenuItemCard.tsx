133fa55141fa4bb11a5e4777a2b41178aecd6f61
Svetoslav Dimitrov <svetoslavdimitrov@Svetoslavs-MacBook-Pro.local>
2025-04-30 14:26:25 +0300

    update the menulistitem

--- client/src/components/MenuItemCard.tsx
+++ client/src/components/MenuItemCard.tsx
@@ -2,16 +2,37 @@ import React from "react";
 import { MenuItem } from "./MenuList";
 import "./MenuItemCard.scss";
 import { useShoppingCart } from "../contexts/ShoppingCartContextProvider";
+import { PlusIcon, MinusIcon } from "@heroicons/react/24/solid";
+import '../tailwind-server.css'
 
 type Props = {
   item: MenuItem;
 };
 
 const MenuItemCard: React.FC<Props> = ({ item }) => {
-  const { addToCart } = useShoppingCart();
+  const { cartItems, addToCart, removeFromCart, updateQuantity } = useShoppingCart();
+
+  const getItemQuantity = () => {
+    const cartItem = cartItems.find((i) => i.id === item.id);
+    return cartItem ? cartItem.quantity : 0;
+  };
+
+  const quantity = getItemQuantity();
 
   const onAddItemToCart = () => {
     addToCart({ ...item, quantity: 1 });
+  };
+
+  const onIncrement = () => {
+    addToCart({ ...item, quantity: 1 });
+  };
+
+  const onDecrement = () => {
+    if (quantity > 1) {
+      updateQuantity(item.id, quantity - 1);
+    } else {
+      removeFromCart(item.id);
+    }
   };
 
   return (
@@ -19,12 +40,28 @@ const MenuItemCard: React.FC<Props> = ({ item }) => {
       <h3 className="menu-header">{item.name}</h3>
       <p className="menu-description">{item.description}</p>
       <strong className="menu-description">{item.price.toFixed(2)} lv</strong>
-      {item.allergens && (
-        <p className="menu-allergens">{item.allergens}</p>
+      {item.allergens && item.allergens !== "None" && (
+        <p className="menu-allergens">Allergens: {item.allergens}</p>
       )}
-      <button className="add-to-cart-button" onClick={onAddItemToCart}>
-        Add to cart
-      </button>
+
+      {quantity === 0 ? (
+        <button className="add-to-cart-button" onClick={onAddItemToCart}>
+          Add to cart
+        </button>
+      ) : (
+        <div className="cart-control">
+          <button
+            className="quantity-btn"
+            onClick={onDecrement}
+          >
+            <MinusIcon className="text-black w-5 h-5" />
+          </button>
+          <span className="quantity-count">{quantity}</span>
+          <button className="quantity-btn" onClick={onIncrement}>
+            <PlusIcon className="text-black w-5 h-5" />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- client/src/contexts/ShoppingCartContextProvider.tsx
+++ client/src/contexts/ShoppingCartContextProvider.tsx
@@ -1,65 +1,65 @@
-import React, { createContext, useContext, useState } from "react";
-import { MenuItem } from "../components/MenuList";
+  import React, { createContext, useContext, useState } from "react";
+  import { MenuItem } from "../components/MenuList";
 
-type ShoppingCartContextType = {
-    cartItems: MenuItem[];
-    addToCart: (item: MenuItem) => void;
-    removeFromCart: (id: number) => void;
-    updateQuantity: (id: number, quantity: number) => void;
-    clearCart: () => void;
-};
+  type ShoppingCartContextType = {
+      cartItems: MenuItem[];
+      addToCart: (item: MenuItem) => void;
+      removeFromCart: (id: number) => void;
+      updateQuantity: (id: number, quantity: number) => void;
+      clearCart: () => void;
+  };
 
-const ShoppingCartContext = createContext<ShoppingCartContextType | undefined>(undefined);
+  const ShoppingCartContext = createContext<ShoppingCartContextType | undefined>(undefined);
 
-type ShoppingCartProviderProps = {
-    children: React.ReactNode;
-};
+  type ShoppingCartProviderProps = {
+      children: React.ReactNode;
+  };
 
-export const ShoppingCartContextProvider = ({ children }: ShoppingCartProviderProps) => {
-    const [cartItems, setCartItems] = useState<MenuItem[]>([]);
+  export const ShoppingCartContextProvider = ({ children }: ShoppingCartProviderProps) => {
+      const [cartItems, setCartItems] = useState<MenuItem[]>([]);
 
-    const addToCart = (item: MenuItem) => {
-        setCartItems((prev) => {
-          const existingItem = prev.find((cartItem) => cartItem.id === item.id);
-          if (existingItem) {
-            return prev.map((cartItem) =>
-              cartItem.id === item.id
-                ? { ...cartItem, quantity: cartItem.quantity + 1 }
-                : cartItem
-            );
-          } else {
-            return [...prev, { ...item, quantity: 1 }];
-          }
-        });
-      };      
+      const addToCart = (item: MenuItem) => {
+          setCartItems((prev) => {
+            const existingItem = prev.find((cartItem) => cartItem.id === item.id);
+            if (existingItem) {
+              return prev.map((cartItem) =>
+                cartItem.id === item.id
+                  ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                  : cartItem
+              );
+            } else {
+              return [...prev, { ...item, quantity: 1 }];
+            }
+          });
+        };      
 
-    const removeFromCart = (id: number) => {
-        setCartItems((prev) => prev.filter((item) => item.id !== id));
-    };
-
-    const updateQuantity = (id: number, quantity: number) => {
-        setCartItems((currentItems) =>
-          currentItems.map((item) =>
-            item.id === id ? { ...item, quantity: Number(quantity) } : item
-          )
-        );
+      const removeFromCart = (id: number) => {
+          setCartItems((prev) => prev.filter((item) => item.id !== id));
       };
 
-    const clearCart = () => {
-        setCartItems([]);
-    }
+      const updateQuantity = (id: number, quantity: number) => {
+          setCartItems((currentItems) =>
+            currentItems.map((item) =>
+              item.id === id ? { ...item, quantity: Number(quantity) } : item
+            )
+          );
+        };
 
-    return (
-        <ShoppingCartContext.Provider value={{ cartItems, addToCart, removeFromCart, updateQuantity, clearCart }}>
-            {children}
-        </ShoppingCartContext.Provider>
-    );
-};
+      const clearCart = () => {
+          setCartItems([]);
+      }
 
-export const useShoppingCart = () => {
-    const context = useContext(ShoppingCartContext);
-    if (!context) {
-        throw new Error("useShoppingCart must be used within a ShoppingCartContextProvider");
-    }
-    return context;
-};
+      return (
+          <ShoppingCartContext.Provider value={{ cartItems, addToCart, removeFromCart, updateQuantity, clearCart }}>
+              {children}
+          </ShoppingCartContext.Provider>
+      );
+  };
+
+  export const useShoppingCart = () => {
+      const context = useContext(ShoppingCartContext);
+      if (!context) {
+          throw new Error("useShoppingCart must be used within a ShoppingCartContextProvider");
+      }
+      return context;
+  };
